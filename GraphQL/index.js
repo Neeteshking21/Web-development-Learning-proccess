275de5cf2e20003dca4aba9c48e9d1c00d22512d
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-26 12:33:35 +0530

    Update readme.md file and create express templete

---
+++ GraphQL/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res)=> res.send("Home page"))
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
