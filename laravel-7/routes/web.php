1d79191482724969c6cf88cd6cda9d3e6f4cfa82
Neetesh <neeteshking21@gmail.com>
2021-11-10 23:59:01 +0530

    Added route to laravel-7

--- laravel-7/routes/web.php
+++ laravel-7/routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+}); 
+
+Route::get('/post/{id}/{cars}', function($id, $cars){
+    return "The employee ".$id ." has ".$cars . " cars";
 });
